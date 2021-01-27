7390ea6085f82c02e01ce885840f1ce5ccc6d8ec
syee <stevenyee64@gmail.com>
2021-01-26 23:39:00 -0800

    Save synthetic control and treated averages

--- monteCarloSimplified.R
+++ monteCarloSimplified.R
@@ -49,6 +49,8 @@ ri <- function(observed,T0,T1){
     RMSPE = rep(NA,J1)
     T_stat = rep(NA,J1)
     Post_treatment_avg = rep(NA,J1)
+    actual_synth_control = rep(NA,T1)
+    actual_treated = rep(NA,T1)
     
     # RI requires looping over all units (assigning each one the treatment status)
     for (j in 1:(J1)){
@@ -75,6 +77,11 @@ ri <- function(observed,T0,T1){
         T_stat[j] = abs((average_treat/(T1-T0))/(std_treat/sqrt(T1-T0)))
 
         Post_treatment_avg[j] = mean(abs(effect[(T0+1):T1]))
+        
+        if(j==1){
+            actual_synth_control = synth_control
+            actual_treated = treated
+        }
     }
     
     # Pvalue for each test stat is proportion of placebos with a greater test stat
@@ -82,7 +89,7 @@ ri <- function(observed,T0,T1){
     pvalue_RMPSE = mean(RMSPE>=RMSPE[1])
     pvalue_tstat = mean(T_stat >= T_stat[1])
     pvalue_post = mean(Post_treatment_avg >= Post_treatment_avg[1])
-    return(list(pvalue_RMSPE=pvalue_RMPSE, pvalue_tstat=pvalue_tstat, pvalue_post=pvalue_post))
+    return(list(pvalue_RMSPE=pvalue_RMPSE, pvalue_tstat=pvalue_tstat, pvalue_post=pvalue_post, actual_treated=actual_treated, actual_synth_control=actual_synth_control))
 }
 
 gen_data<- function(case, J1, T1){
@@ -165,14 +172,15 @@ T1  = 25
 T0 = 15
 J0 = 19
 J1 = J0 + 1
-sims = 200
-lambda_vals = 5
-lambda_start = 0
+sims = 1000
+lambda_vals = 0
+size_vals = J1
+lambda_start = 1
 lambda_end = 1
 size = 0.10
 
 case = 1
-varying = FALSE
+varying = TRUE
 
 simulate <- function(case,sims,lambda_vals,lambda_start,lambda_end,varying, T0,T1,J0,J1){
     
@@ -180,6 +188,8 @@ simulate <- function(case,sims,lambda_vals,lambda_start,lambda_end,varying, T0,T
     pvalue_RMSPE_mat = matrix(NA,sims,lambda_vals+1)
     pvalue_tstat_mat = matrix(NA,sims,lambda_vals+1)
     pvalue_post_mat = matrix(NA,sims,lambda_vals+1)
+    synth_sum = matrix(0, lambda_vals+1, T1)
+    treated_sum = matrix(0, lambda_vals+1, T1)
     
     for (iter1 in 1:(lambda_vals+1)){
         lambda_test = lambda_seq[iter1]
@@ -197,12 +207,22 @@ simulate <- function(case,sims,lambda_vals,lambda_start,lambda_end,varying, T0,T
             pvalue_RMSPE_mat[iter2, iter1] = results$pvalue_RMSPE
             pvalue_tstat_mat[iter2, iter1] = results$pvalue_tstat
             pvalue_post_mat[iter2, iter1] = results$pvalue_post
+            
+            synth_sum[iter1,] = synth_sum[iter1,] + results$actual_synth_control
+            treated_sum[iter1,] = treated_sum[iter1,] + results$actual_treated
         }
     }
-    return(list(pvalue_RMSPE_mat=pvalue_RMSPE_mat, pvalue_tstat_mat=pvalue_tstat_mat,pvalue_post_mat=pvalue_post_mat))
+
+    synth_avg = synth_sum / sims
+    treated_avg = treated_sum / sims
+    return(list(pvalue_RMSPE_mat=pvalue_RMSPE_mat, pvalue_tstat_mat=pvalue_tstat_mat,pvalue_post_mat=pvalue_post_mat, synth_avg=synth_avg, treated_avg=treated_avg))
 }
 
 simulation = simulate(case,sims,lambda_vals,lambda_start,lambda_end, varying, T0,T1,J0,J1)
+
+plot(linspace(1,T1,T1), simulation$treated_avg[1,], type = "l")
+lines(linspace(1,T1,T1), simulation$synth_avg[1,], type="l")
+
 
 gen_power_curve <- function(case, varying, lambda_start,lambda_end, pvalue_RMSPE_mat, pvalue_tstat_mat, pvalue_post_mat, size){
     lambda_vals = dim(pvalue_RMSPE_mat)[2] - 1
@@ -223,9 +243,27 @@ gen_power_curve <- function(case, varying, lambda_start,lambda_end, pvalue_RMSPE
            col=c("red", "blue","black"),lty=1:1, cex=0.8)
 }
 
-gen_power_curve(case, varying, lambda_start,lambda_end, simulation$pvalue_RMSPE_mat, simulation$pvalue_tstat_mat, simulation$pvalue_post_mat, size)
+check_size_control <-function(case, varying,lambda_vals,lambda_start,lambda_end, pvalue_RMSPE_mat, pvalue_tstat_mat, pvalue_post_mat,size_vals){
+    size_seq = linspace(0,1,size_vals+1)
+    pvalue_plot = matrix(NA,size_vals+1,4)
+    
+    for (i in 0:size_vals+1){
+        pvalue_plot[i,1]=size_seq[i]
+        pvalue_plot[i,2]=mean(pvalue_RMSPE_mat[,1] <= size_seq[i])
+        pvalue_plot[i,3]=mean(pvalue_tstat_mat[,1] <= size_seq[i])
+        pvalue_plot[i,4]=mean(pvalue_post_mat[,1] <= size_seq[i])
+    }
+    
+    plot(pvalue_plot[,1],pvalue_plot[,2],type="l",col="red")
+    lines(pvalue_plot[,1],pvalue_plot[,3],type="l",col="blue")
+    lines(pvalue_plot[,1],pvalue_plot[,4],type="l",col="black")
+    legend("bottomright", legend=c("RMSPE", "T-stat","Post-Treatment"),
+           col=c("red", "blue","black"),lty=1:1, cex=0.8)
+}
 
+# gen_power_curve(case, varying, lambda_start,lambda_end, simulation$pvalue_RMSPE_mat, simulation$pvalue_tstat_mat, simulation$pvalue_post_mat, size)
 
+check_size_control(case, varying,lambda_vals,lambda_start,lambda_end, simulation$pvalue_RMSPE_mat, simulation$pvalue_tstat_mat, simulation$pvalue_post_mat,size_vals)
 
 
 
